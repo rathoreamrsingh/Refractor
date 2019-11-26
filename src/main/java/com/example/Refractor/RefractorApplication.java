6604af6948fb478ca38035b27ce6051a8abee6c9
Amar Singh <amarsingh@outlook.in>
2019-11-26 23:43:02 +0800

    Amar|Git initial setup

---
+++ src/main/java/com/example/Refractor/RefractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Refractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Refractor/RefractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Refractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
